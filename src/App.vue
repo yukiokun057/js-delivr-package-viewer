0d831d98638104d608d4b819af3e11f82e886389
Danil Danilchenko <danilchenkodb@gmail.com>
2020-07-06 02:52:58 +0700

    Project created

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    // TODO: place here search input component
+    // TODO: place here package viewer component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {},
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import vuetify from '@/plugins/vuetify';
+import store from './store';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
