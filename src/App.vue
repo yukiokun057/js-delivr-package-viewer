fe79ec0e8c0cd99fbf9f57467c94612e56cce3e0
Danil Danilchenko <danilchenkodb@gmail.com>
2020-07-09 04:16:53 +0700

    Added footer with author info

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <v-app>
-    <WidgetSearch />
-    <WidgetPackages @item-clicked="itemClicked" />
-    <DialogPackageInfo ref="dialog-package-info" />
+    <v-main>
+      <WidgetSearch />
+      <WidgetPackages @item-clicked="itemClicked" />
+      <DialogPackageInfo ref="dialog-package-info" />
+    </v-main>
+    <v-footer app color="#fafafa">
+      <div class="author-container author--text">
+        <a href="https://gitlab.com/danildanil_" class="author-link author--text author--text-link">
+          <v-img
+            src="https://about.gitlab.com/images/press/logo/png/gitlab-icon-rgb.png"
+            height="32"
+            width="20"
+            alt="gitlab-icon"
+          />Danil Danilchenko,
+        </a>
+        2020
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
@@ -24,6 +39,27 @@ export default {
 </script>
 
 <style lang="scss">
+.author-container {
+  display: flex;
+  align-items: center;
+  .author-link {
+    margin-right: 4px;
+    display: flex;
+    align-items: center;
+    .v-image {
+      margin-right: 4px;
+    }
+  }
+}
+.author--text {
+  font-size: 16px;
+  font-weight: 600;
+  color: #000 !important;
+}
+.author--text-link {
+  text-decoration: none;
+}
+
 #app {
   font-family: Nunito, sans-serif;
   -webkit-font-smoothing: antialiased;
